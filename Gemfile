f7aa8536936fb237381576df5b2ecab7dc98e0a3
Clement Choi <clementchoi1997@gmail.com>
2022-12-02 01:19:23 +0900

    basic search bar etc.

--- Gemfile
+++ Gemfile
@@ -82,3 +82,4 @@ gem "simple_form", github: "heartcombo/simple_form"
 gem 'jquery-rails'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.16.0'
+gem "pg_search"
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,11 @@ class Recipe < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+  against: [ :name ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
