088d8e4ba4b2bd935edd434fb3f9296318e21920
Clement Choi <clementchoi1997@gmail.com>
2022-12-20 18:16:50 +0900

    seeding ratings

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :ratings
   has_one_attached :photo
 
   include PgSearch::Model
