688d579ace89ece2cba719df1fd548c1e37723da
Clement Choi <clementchoi1997@gmail.com>
2022-12-20 19:13:07 +0900

    display stars on show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_one_attached :photo
 
   include PgSearch::Model
